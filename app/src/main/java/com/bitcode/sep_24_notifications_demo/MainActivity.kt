05c12e938e2758196dfa07a5cd2af3d189c365f8
aishwarya <ashp17star@gmail.com>
2024-11-14 16:46:44 +0530

    Notifications Demo Updated

--- app/src/main/java/com/bitcode/sep_24_notifications_demo/MainActivity.kt
+++ app/src/main/java/com/bitcode/sep_24_notifications_demo/MainActivity.kt
@@ -5,8 +5,13 @@ import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationChannelCompat
@@ -15,6 +20,7 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bitcode.sep_24_notifications_demo.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
@@ -27,6 +33,7 @@ class MainActivity : AppCompatActivity() {
     private val SNACK_BAR_NOTIFICATION = 4
     private val BIG_PICTURE_STYLE_NOTIFICATION = 5
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,6 +42,26 @@ class MainActivity : AppCompatActivity() {
         createNotificationChannel()
         activityMainBinding.btnSimpleNotification.setOnClickListener {
             simpleNotification()
+        }
+
+        activityMainBinding.btnInboxStyleNotification.setOnClickListener {
+            inboxStyleNotification()
+        }
+
+        activityMainBinding.btnActionStyleNotification.setOnClickListener {
+            actionTextStyleNotification()
+        }
+
+        activityMainBinding.btnSnackBarNotification.setOnClickListener {
+            snackBarNotification()
+        }
+
+        activityMainBinding.btnBigPictureStyleNotification.setOnClickListener {
+            bigPictureStyleNotification()
+        }
+
+        activityMainBinding.btnSnackBarNotification.setOnClickListener {
+            snackBarNotification()
         }
     }
 
@@ -61,19 +88,90 @@ class MainActivity : AppCompatActivity() {
         notificationManager.notify(SIMPLE_NOTIFICATION,notificationBuilder.build())
     }
 
+    @SuppressLint("MissingPermission")
     private fun inboxStyleNotification(){
+        var notificationBuilder = NotificationCompat.Builder(this,channel_id)
+        notificationBuilder.setContentTitle("New Upcoming Batches")
+        notificationBuilder.setColor(155)
+        notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH)
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_background)
 
+        var inboxStyleNotification = NotificationCompat.InboxStyle()
+        inboxStyleNotification.addLine("iOS - Nov 24")
+        inboxStyleNotification.addLine("Android - Nov 24")
+        inboxStyleNotification.addLine("Web - Nov 24")
+        inboxStyleNotification.setSummaryText("Contact Bitcode For More Information")
+        inboxStyleNotification.setBigContentTitle("Bitcode Technologies Batches Schedule")
+
+        notificationBuilder.setStyle(inboxStyleNotification)
+
+        notificationManager.notify(INBOX_STYLE_NOTIFICATION, inboxStyleNotification.build()!!)
     }
 
-    fun actionTextStyleNotification(){
+    @SuppressLint("MissingPermission")
+    private fun actionTextStyleNotification(){
+        var notificationBuilder = NotificationCompat.Builder(this,channel_id)
+        notificationBuilder.setContentTitle("Action text Style")
+        notificationBuilder.setPriority(NotificationCompat.PRIORITY_LOW)
+        notificationBuilder.setColor(200)
+        notificationBuilder.setAutoCancel(false)
+        notificationBuilder.setLights(200,50,50)
+        notificationBuilder.setOngoing(true)
+//        var bitmapIcon = BitmapFactory.decodeResource(resources,R.drawable.ic_launcher_background)
+//        notificationBuilder.setLargeIcon(bitmapIcon)
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+        var intent = Intent(this,SecondActivity::class.java)
+        var pendingIntent = PendingIntent.getActivity(this,
+            1,
+            intent,
+            PendingIntent.FLAG_MUTABLE)
 
+        var actionTextStyle = NotificationCompat.Action(
+            R.drawable.ic_launcher_background,
+            "Action Text Style",
+            pendingIntent
+        )
+
+        notificationBuilder.addAction(actionTextStyle)
+
+        notificationManager.notify(ACTION_TEXT_STYLE_NOTIFICATION,notificationBuilder.build())
     }
 
-    fun snackBarNotification(){
-
-    }
-
+    @RequiresApi(Build.VERSION_CODES.S)
+    @SuppressLint("MissingPermission")
     fun bigPictureStyleNotification(){
+        var notificationBuilder = NotificationCompat.Builder(this,channel_id)
+        notificationBuilder.setPriority(NotificationCompat.PRIORITY_MIN)
+        notificationBuilder.setColor(100)
+        notificationBuilder.setVibrate(LongArray(10) { index -> (index + 10).toLong() })
+        notificationBuilder.setCategory(NotificationCompat.CATEGORY_MESSAGE)
+        notificationBuilder.setContentTitle("Big Picture Style Notification")
+        notificationBuilder.setOngoing(true)
+        notificationBuilder.setContentText("This is Android Batch Sep 24")
+        notificationBuilder.setLights(144,50,50)
+        notificationBuilder.setSmallIcon(R.drawable.ic_launcher_background)
+        var bigPictureStyle = NotificationCompat.BigPictureStyle()
+        bigPictureStyle.setBigContentTitle("Bitcode Youtube Channel")
+
+        var bitmapIcon = BitmapFactory.decodeResource(resources,R.drawable.test_image_3)
+//        activityMainBinding.imgView.setImageBitmap(bitmapIcon)
+
+        bigPictureStyle.bigPicture(bitmapIcon)
+
+//        bigPictureStyle.bigLargeIcon(Bitmap.createBitmap(BitmapFactory.decodeResource(resources,R.drawable.test_image_3)))
+
+        bigPictureStyle.setContentDescription("This channel is for videos " +
+                "on technologies like iOS, Android, Web")
+        notificationManager.notify(BIG_PICTURE_STYLE_NOTIFICATION,notificationBuilder.build())
+    }
+
+    @SuppressLint("MissingPermission")
+    fun snackBarNotification(){
+//        var snackBar = Snackbar.make(this,
+//
+//            "Snack Bar",
+//            Snackbar.LENGTH_LONG)
+//        snackBar.setText("Snack bar")
 
     }
 
